578bb2deed403f2c64c82619daa5f99f76d52802
Viet Dung Nguyen <dnguyen98@protonmail.com>
2018-06-02 20:01:21 +0700

    Change variable name msg to error_msg

--- app/forms.py
+++ app/forms.py
@@ -7,14 +7,13 @@ from app.models import Code
 import re
 """Web forms of the app"""
 
-msg = {
+error_msg = {
     'URL':
         'Not a valid URL',
     'Code length':
         'The code must contain 1-6 character',
     'Code character':
-        'The code can only contain\
- lower case character and digits',
+        'The code can only contain lower case character and digits',
     'Code unique':
         'This code is already used'
 }
@@ -25,22 +24,22 @@ class GetCustomURLForm(FlaskForm):
         'URL',
         validators=[
             DataRequired(),
-            URL(require_tld=False, message=msg['URL'])
+            URL(require_tld=False, message=error_msg['URL'])
         ])
     customcode = StringField(
         'Code',
         validators=[
             DataRequired(),
-            Length(min=1, max=6, message=msg['Code length'])
+            Length(min=1, max=6, message=error_msg['Code length'])
         ])
     submit = SubmitField('Submit')
 
     def validate_customcode(self, customcode):
         if re.match("^[\da-z]+$", customcode.data) is None:
-            raise ValidationError(msg['Code character'])
+            raise ValidationError(error_msg['Code character'])
         c = Code.query.filter_by(code=customcode.data).first()
         if c is not None:
-            raise ValidationError(msg['Code unique'])
+            raise ValidationError(error_msg['Code unique'])
 
 
 class GetURLForm(FlaskForm):
@@ -48,6 +47,6 @@ class GetURLForm(FlaskForm):
         'URL',
         validators=[
             DataRequired(),
-            URL(require_tld=False, message=msg['URL'])
+            URL(require_tld=False, message=error_msg['URL'])
         ])
     submit = SubmitField('Get code')
